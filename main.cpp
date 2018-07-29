8bb4ec5ef82a999e817f068f6501f1d538e3bebb
tagirmi <tagirmi@gmail.com>
2018-07-29 17:52:40 +0300

    добавил файлы проекта

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+  try {
+    //TODO ...
+  }
+  catch(const std::exception& e) {
+    std::cerr << e.what() << std::endl;
+  }
+
+  return 0;
+}
